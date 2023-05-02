951ff330f28f9914fafe38dcbb4eb3c562b794e2
Brandon Lee <124757679+bjlee97@users.noreply.github.com>
2023-05-01 18:07:27 -0700

    added script

--- script.js
+++ script.js
@@ -24,3 +24,13 @@ function success(position){         //enable permissions
 	// Print out the latitude and longitude to see if it works!
 }
     
+const btn = document.getElementById('getWeatherBtn')
+
+btn.addEventListener("click", () => {
+    let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i = 0; i < forecast.length; i++) {
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0 F";
+      }
+});
+
